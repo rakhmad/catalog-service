b4dc962f30ec05645db662a5a35a01339d8e9da3
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2021-06-23 13:59:23 +0700

    Adding REST API for getting all items.

--- src/main/java/id/redhat/demo/catalog/CatalogItemController.java
+++ src/main/java/id/redhat/demo/catalog/CatalogItemController.java
@@ -1,14 +1,25 @@
 package id.redhat.demo.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CatalogItemController {
-    private static final String defaultResponse = "Hello from Catalog";
+    private static final String DEFAULT_RESPONSE = "Hello from Catalog";
+
+    @Autowired
+    private CatalogItemRepository catalogItemRepository;
 
     @GetMapping("/v1/default")
     public String getDefaultResponse() {
-        return defaultResponse;
+        return DEFAULT_RESPONSE;
+    }
+
+    @GetMapping("/v1/catalog/items")
+    public @ResponseBody Iterable<CatalogItem> getAllCatalogItems() {
+        return catalogItemRepository.findAll();
     }
 }
