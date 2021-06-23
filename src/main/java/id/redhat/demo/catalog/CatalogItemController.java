bae8b33e0c0ade0ffd2e77036da226b2359fe1c2
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2021-06-23 12:02:58 +0700

    Initial REST Controller.

--- src/main/java/id/redhat/demo/catalog/CatalogItemController.java
+++ src/main/java/id/redhat/demo/catalog/CatalogItemController.java
@@ -1,2 +1,14 @@
-package id.redhat.demo.catalog;public class CatalogItemController {
+package id.redhat.demo.catalog;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CatalogItemController {
+    private static final String defaultResponse = "Hello from Catalog";
+
+    @GetMapping("/v1/default")
+    public String getDefaultResponse() {
+        return defaultResponse;
+    }
 }
