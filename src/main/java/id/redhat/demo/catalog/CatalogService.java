9cce9ea93ae91ba3f3cdcded8f730444265be183
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2021-06-24 12:19:12 +0700

    Use JpaRepository instead of CrudRepository.
    Change to JpaRepository interface because it is more flexible than CrudRepository.
    
    Signed-off-by: Rakhmad Azhari <rakhmad.azhari@gmail.com>

--- src/main/java/id/redhat/demo/catalog/CatalogService.java
+++ src/main/java/id/redhat/demo/catalog/CatalogService.java
@@ -1,2 +1,18 @@
-package id.redhat.demo.catalog;public class CatalogService {
+package id.redhat.demo.catalog;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CatalogService {
+    private final CatalogItemRepository catalogItemRepository;
+
+    public CatalogService(CatalogItemRepository catalogItemRepository) {
+        this.catalogItemRepository = catalogItemRepository;
+    }
+
+    public List<CatalogItem> getAllCatalogItems() {
+        return catalogItemRepository.findAll();
+    }
 }
