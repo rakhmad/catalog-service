1713f2d70d9a2f7ab0c061724e799448156c393e
Rakhmad Azhari <rakhmad.azhari@gmail.com>
2021-06-28 15:45:10 +0700

    Add creating sequence for primary key.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE catalog;
-CREATE USER catalogsvc WITH ENCRYPTED PASSWORD 'r3dh4t1!';
+CREATE USER catalogsvc WITH ENCRYPTED PASSWORD '<CHANGEME>!';
 GRANT ALL PRIVILEGES ON DATABASE catalog TO catalogsvc;
+
+create sequence catalog_item_id_seq;
+alter table catalog_item alter column id set default nextval('public.catalog_item_id_seq');
+alter sequence catalog_item_id_seq owned by catalog_item.id;
